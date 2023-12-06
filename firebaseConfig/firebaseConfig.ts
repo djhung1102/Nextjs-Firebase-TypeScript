afe1f0d00665db2b675f84a0e3238918cc84a1b4
Nguyen Manh Hung <81987040+djhung1102@users.noreply.github.com>
2023-12-06 16:37:45 +0700

    Update firebaseConfig.ts

--- firebaseConfig/firebaseConfig.ts
+++ firebaseConfig/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAg7qzqKnwibNKHtfGQDb8ByIymTxxXFkM",
-  authDomain: "fir-blogging-cc0ee.firebaseapp.com",
-  projectId: "fir-blogging-cc0ee",
-  storageBucket: "fir-blogging-cc0ee.appspot.com",
-  messagingSenderId: "366238865285",
-  appId: "1:366238865285:web:8dcc0f53fce7f22d4a16ec",
+  apiKey: "abc",
+  authDomain: "abc.firebaseapp.com",
+  projectId: "abc",
+  storageBucket: "acb",
+  messagingSenderId: "366",
+  appId: "4a16ec",
 };
 
 const app = initializeApp(firebaseConfig);
